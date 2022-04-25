f1b5fe7b9f0c7f5d1cd0db106e664eee26605ca5
jalaxy <chinajxy@outlook.com>
2022-04-25 14:14:10 +0800

    navigation bar improvement

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -3,29 +3,30 @@
 
 resizePage = () => {
     const navibarHeight = 50, logoWidth = 150;
-    const vertmenuPaddingTop = 16, vertmenuWidth = 200;
+    const vertmenuWidth = 200;
+    const vertmenuBorderWidth = 3;
+    const hideButtonHeight = 50;
+    const menuTabsBottonPadding = 10;
 
     const navibar = document.getElementById('navibar');
     const logo = document.getElementById('logo');
     const vertmenu = document.getElementById('vertmenu');
     const content = document.getElementById('content');
+    const hideButton = document.getElementById('hide-button');
+    const menuTabs = document.getElementById('menu-tabs');
+    const menuTabsBottom = document.getElementById('tabs-bottom');
 
     navibar.style.height = navibarHeight + 'px';
     logo.style.height = navibarHeight + 'px';
     logo.style.width = logoWidth + 'px';
     logo.style.lineHeight = navibarHeight + 'px';
-    vertmenu.style.paddingTop = vertmenuPaddingTop + 'px';
-    vertmenu.style.width = vertmenuWidth + 'px';
-    vertmenu.style.height = window.innerHeight - navibarHeight - vertmenuPaddingTop + 'px';
-
-    //content.style.padding = contentPaddingY + 'px ' + contentPaddingX + 'px ' +
-    //    contentPaddingY + 'px ' + contentPaddingX + 'px';
-    //content.style.margin = contentMarginY + 'px ' + contentMarginX + 'px ' +
-    //    contentMarginY + 'px ' + contentMarginX + 'px';
-    //content.style.borderWidth = '0 0 0 0';
-    //content.style.borderStyle = 'none';
+    vertmenu.style.borderWidth = vertmenuBorderWidth + 'px';
+    vertmenu.style.width = vertmenuWidth - vertmenuBorderWidth * 2 + 'px';
     content.style.width = window.innerWidth - vertmenuWidth + 'px';
     content.style.height = window.innerHeight - navibarHeight + 'px';
+    hideButton.style.height = hideButtonHeight + 'px';
+    menuTabs.style.height = window.innerHeight - navibarHeight - vertmenuBorderWidth * 2 - hideButtonHeight - menuTabsBottonPadding + 'px';
+    menuTabsBottom.style.height = menuTabsBottonPadding + 'px';
 }
 
 window.addEventListener('resize', resizePage);
