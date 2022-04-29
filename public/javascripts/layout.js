a3e4b8e08884a945ffbb6b6053b631db635677e3
jalaxy <chinajxy@outlook.com>
2022-04-29 15:34:56 +0800

    hide side menu (with cookie)

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -6,6 +6,7 @@ resizePage = () => {
     const vertmenuWidth = 200;
     const vertmenuBorderWidth = 3;
     const hideButtonHeight = 50;
+    const hideButtonPadding = 8, hideButtonMargin = 8, hideButtonBorder = 2;
     const menuTabsBottonPadding = 10;
 
     const navibar = document.getElementById('navibar');
@@ -13,6 +14,9 @@ resizePage = () => {
     const vertmenu = document.getElementById('vertmenu');
     const content = document.getElementById('content');
     const hideButton = document.getElementById('hide-button');
+    const hideButtonImg = document.getElementById('hide-btn-img');
+    const showButton = document.getElementById('show-button');
+    const showButtonImg = document.getElementById('show-btn-img');
     const menuTabs = document.getElementById('menu-tabs');
     const menuTabsBottom = document.getElementById('tabs-bottom');
 
@@ -25,8 +29,46 @@ resizePage = () => {
     content.style.width = window.innerWidth - vertmenuWidth + 'px';
     content.style.height = window.innerHeight - navibarHeight + 'px';
     hideButton.style.height = hideButtonHeight + 'px';
+    hideButtonImg.style.height = hideButtonHeight -
+        (hideButtonPadding + hideButtonMargin + hideButtonBorder) * 2 + 'px';
+    hideButtonImg.style.padding = hideButtonPadding + 'px';
+    hideButtonImg.style.margin = hideButtonMargin + 'px';
+    hideButtonImg.style.borderWidth = hideButtonBorder + 'px';
+    showButton.style.width = hideButtonHeight - 2 * hideButtonMargin + 'px';
+    showButton.style.height = window.innerHeight - navibarHeight + 'px';
+    showButtonImg.style.height = hideButtonHeight -
+        (hideButtonPadding + hideButtonMargin + hideButtonBorder) * 2 + 'px';
+    showButtonImg.style.padding = hideButtonPadding + 'px';
+    showButtonImg.style.margin = hideButtonMargin + 'px';
+    showButtonImg.style.marginLeft = showButtonImg.style.marginRight = '0';
     menuTabs.style.height = window.innerHeight - navibarHeight - vertmenuBorderWidth * 2 - hideButtonHeight - menuTabsBottonPadding + 'px';
     menuTabsBottom.style.height = menuTabsBottonPadding + 'px';
 }
 
-window.addEventListener('resize', resizePage);
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+showVert = (show) => {
+    setCookie('hide', 'true', show ? 0 : 1);
+    document.getElementById('vertmenu').style.display = show ? 'unset' : 'none';
+    document.getElementById('show-button').style.display = show ? 'none' : 'unset';
+}
